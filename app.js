01263b64cc5e37b4693bf0651fe255a6ed7999c2
kulaszewicz <36889025+kulaszewicz@users.noreply.github.com>
2019-12-29 18:34:36 +0100

    basic Router for energy done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const bodyEnergyRouter = require('./src/controllers/bodyEnergy/get');
+const energy = require('./src/controllers/energy');
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/workflow/energy/body/', bodyEnergyRouter);
+app.use('/workflow/energy', energy);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- src/controllers/bodyEnergy/get.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET user body energy */
-router.get('/workflow/energy/body/', (req, res) => {
-    res.json({ body: 100 });
-});
-
-module.exports = router;
