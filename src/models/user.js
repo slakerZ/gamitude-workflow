3e76f687ca9713c1980bfec184294ad91a109413
kulaszewicz <s17838@pjwstk.edu.pl>
2020-01-17 13:17:52 +0100

    User model done

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    firebaseId: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    displayName: {
+        type: String,
+        trim: true,
+    }
+});
+
+model.exports = User;
