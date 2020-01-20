f78394a14ef4b139472bf10df64bfe387233e264
Jakub Kulaszewicz <36889025+kulaszewicz@users.noreply.github.com>
2020-01-20 15:17:15 +0100

    Feature/is72/user workflow (#7)
    * User model done
    
    * User Controller done

---
+++ src/controllers/user/post.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const User = require('../../models/user');
+const router = express.Router();
+
+/* POST new user */
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+});
+
+module.exports = router;
