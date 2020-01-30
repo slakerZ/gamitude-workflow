25d095f6d6e20d8a0fd8342933c8297022181084
Rustyiller <s17707@pjwstk.edu.pl>
2020-01-30 15:06:24 +0100

    post insteed of patch

--- src/controllers/energy/update.js
+++ src/controllers/energy/update.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const Energy = require('../../models/energy');
 const router = express.Router();
-const { estimateEnergyLoss } = require("../../utils/energyEstimates");
+const { estimateEnergyLoss } = require('../../utils/energyEstimates');
 
 /* PATCH user energy */
-router.patch('/', async (req, res) => {
-    const {project, projectUsage} = req.body;
-    const currEnergy = await Energy.findOne({userId: req.body.project.userId});
-    const energyUpdated = estimateEnergyLoss(project.stats, project.dominantStat, projectUsage.timeSpend, currEnergy);
+router.post('/', async (req, res) => {
+    const { project, projectUsage } = req.body;
+    const currEnergy = await Energy.findOne({
+        userId: req.body.project.userId,
+    });
+    const energyUpdated = estimateEnergyLoss(
+        project.stats,
+        project.dominantStat,
+        projectUsage.timeSpend,
+        currEnergy
+    );
     try {
-        const energy = await Energy.findOneAndUpdate({userId: req.body.project.userId}, energyUpdated, { new: true, runValidators: true});
-        energy ? res.send(energy) : res.status(404).send({error: "User not found!"});
+        const energy = await Energy.findOneAndUpdate(
+            { userId: req.body.project.userId },
+            energyUpdated,
+            { new: true, runValidators: true }
+        );
+        energy
+            ? res.send(energy)
+            : res.status(404).send({ error: 'User not found!' });
     } catch (err) {
         res.status(400).send();
     }
