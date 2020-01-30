f7b854442b488981ea3581aaf9aced96fdcad774
Stanisław Lutkiewicz <Stanislaw.lutkiewicz@softgent.com>
2020-01-30 16:16:49 +0100

    rank init

--- src/controllers/user/post.js
+++ src/controllers/user/post.js
@@ -10,8 +10,18 @@ router.post('/', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save();
-
+        axios
+            .post(ranksUrl + '/userRank/' + user.firebaseId, {
+                userId: user.firebaseId,
+            })
+            .then(function(response) {
+                res.status(201).send(user);
+            })
+            .catch(function(error) {
+                res.status(400).send();
+            });
         res.status(201).send(user);
+
     } catch (e) {
         res.status(400).send();
     }
