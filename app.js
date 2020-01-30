9bf4c4b6580e6975b0e39b70df45340ff5c56be1
Rustyiller <s17707@pjwstk.edu.pl>
2020-01-30 16:09:30 +0100

    cors

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use('/workflow/energy', energy);
 app.use('/workflow/user', user);
