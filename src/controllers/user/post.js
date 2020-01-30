e4f35ff8f5385de7376ad012b4f826c6c7563f83
kulaszewicz <36889025+kulaszewicz@users.noreply.github.com>
2020-01-30 17:35:15 +0100

    create user energy object on user creation

--- src/controllers/user/post.js
+++ src/controllers/user/post.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../../models/user');
+const Energy = require('../../models/energy');
 const router = express.Router();
 const axios = require('axios');
 
@@ -8,8 +9,10 @@ const ranksUrl = 'https://gamitude-ranks.herokuapp.com';
 /* POST new user */
 router.post('/', async (req, res) => {
     const user = new User(req.body);
+    const energy = new Energy({body: 100, mind: 100, emotional: 100, soul: 100, userId: user.firebaseId});
     try {
-        await user.save(); 
+        await user.save();
+        await energy.save();
         axios
             .post(ranksUrl + '/userRank/' + user.firebaseId, {
                 userId: user.firebaseId,
